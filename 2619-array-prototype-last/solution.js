3246275b0b616a5350443e8502f5aa0a119d0eba
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:18:07 +0530

    sync: Array Prototype Last (javascript)

---
+++ 2619-array-prototype-last/solution.js
@@ -0,0 +1,16 @@
+// Solution: Array Prototype Last
+// Runtime: 46 ms | Memory: 53.3 MB
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length === 0) return -1;
+
+    return this[this.length - 1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
