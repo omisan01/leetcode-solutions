54d211259a80c462038cf385c4573b1a02140076
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:21:22 +0530

    sync: Middle of the Linked List (typescript)

---
+++ 0876-middle-of-the-linked-list/solution.ts
@@ -0,0 +1,26 @@
+// Solution: Middle of the Linked List
+// Runtime: 0 ms | Memory: 54.8 MB
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let slow = head;
+    let fast = head;
+
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next
+    }
+
+    return slow
+};
