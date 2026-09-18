b63282ad00113d4387bf85592d1ee847eb6ea17f
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:15:40 +0530

    sync: Maximum Depth of Binary Tree (javascript)

---
+++ 0104-maximum-depth-of-binary-tree/solution.js
@@ -0,0 +1,19 @@
+// Solution: Maximum Depth of Binary Tree
+// Runtime: 1 ms | Memory: 61.3 MB
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
