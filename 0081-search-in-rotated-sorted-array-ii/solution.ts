484ed327a50f99426f5cdc15f641c1b76780b01e
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:21:40 +0530

    sync: Search in Rotated Sorted Array II (typescript)

---
+++ 0081-search-in-rotated-sorted-array-ii/solution.ts
@@ -0,0 +1,31 @@
+// Solution: Search in Rotated Sorted Array II
+// Runtime: 0 ms | Memory: 55.9 MB
+
+function search(nums: number[], target: number): boolean {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right){
+        const mid = Math.floor((left + right) / 2);
+        if(nums[mid] === target){
+            return true
+        }
+        if(nums[mid] === nums[left] && nums[mid] === nums[right]){
+            left++
+            right--
+        }
+        else if(nums[left] <= nums[mid]){
+            if(nums[left] <= target && target < nums[mid]){
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return false
+};
