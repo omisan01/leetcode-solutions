8e80ebdb2bf036721b3552f2be46e53207224aa8
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:15:15 +0530

    sync: Function Composition (javascript)

---
+++ 2629-function-composition/solution.js
@@ -0,0 +1,24 @@
+// Solution: Function Composition
+// Runtime: 56 ms | Memory: 55.1 MB
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    if(functions.length === 0){
+        return (x) => x
+    }
+    return function(x) {
+        let result = x;
+        for(let i = functions.length - 1; i >= 0; i--){
+            result = functions[i]((result))
+        }
+        return result
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
