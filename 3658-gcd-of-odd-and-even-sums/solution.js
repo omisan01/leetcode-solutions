cb857d76641b39e40f8410aeb4c444ef8cf1288c
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:13:56 +0530

    sync: GCD of Odd and Even Sums (javascript)

---
+++ 3658-gcd-of-odd-and-even-sums/solution.js
@@ -0,0 +1,10 @@
+// Solution: GCD of Odd and Even Sums
+// Runtime: 0 ms | Memory: 55.8 MB
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    return n
+};
