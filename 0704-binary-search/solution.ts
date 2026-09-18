840c1a03eea702c2caca1c3c88191b1ed0932126
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:22:03 +0530

    sync: Binary Search (typescript)

---
+++ 0704-binary-search/solution.ts
@@ -0,0 +1,20 @@
+// Solution: Binary Search
+// Runtime: 0 ms | Memory: 58.7 MB
+
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1
+
+    while (left <= right){
+        const mid = Math.floor((left + right) / 2);
+        if(nums[mid] === target){
+            return mid
+        } else if(nums[mid] < target){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return -1
+};
