b1c269ef570d9dc1b0beb79098dcb472dcc90d79
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:21:25 +0530

    sync: Reverse Linked List (typescript)

---
+++ 0206-reverse-linked-list/solution.ts
@@ -0,0 +1,27 @@
+// Solution: Reverse Linked List
+// Runtime: 0 ms | Memory: 56.6 MB
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let curr = head;
+    while (curr !== null){
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr
+        curr = temp
+    }
+        return prev
+
+};
