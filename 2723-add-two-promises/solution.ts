96effebd266f5ccc9fc9dea85bd55c04a6540c2b
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:16:58 +0530

    sync: Add Two Promises (typescript)

---
+++ 2723-add-two-promises/solution.ts
@@ -0,0 +1,16 @@
+// Solution: Add Two Promises
+// Runtime: 55 ms | Memory: 56 MB
+
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    const val1 = await promise1;
+    const val2 = await promise2;
+
+    return val1 + val2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
