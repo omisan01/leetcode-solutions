577c2ac625c47f0360d20a4f736f3fed7cd3b4b7
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-18 15:15:25 +0530

    sync: Counter II (javascript)

---
+++ 2665-counter-ii/solution.js
@@ -0,0 +1,29 @@
+// Solution: Counter II
+// Runtime: 52 ms | Memory: 58.2 MB
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment : () => {
+            return ++count
+        },
+        decrement : () => {
+            return --count
+        },
+        reset : () => {
+            count = init;
+            return count
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
