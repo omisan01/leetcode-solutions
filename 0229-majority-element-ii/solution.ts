b4ef4970b4280d811a52fd5e24223a97e2c70795
Omkar Mitake <80113993+omisan01@users.noreply.github.com>
2026-09-22 15:56:00 +0530

    sync: Majority Element II [1 ms]

--- 0229-majority-element-ii/solution.ts
+++ 0229-majority-element-ii/solution.ts
@@ -1,22 +1,45 @@
 // Solution: Majority Element II
-// Runtime: 9 ms | Memory: 62.5 MB
+// Time: 1 ms (94.12%) | Memory: 60.2 MB (59.66%)
 
 function majorityElement(nums: number[]): number[] {
-    const map = new Map();
-    const majority = Math.floor(nums.length / 3) + 1;
-    const res = []
+    let candidate1 = 0;
+    let candidate2 = 0;
 
-    for (const num of nums){
-        if(map.has(num)){
-            map.set(num, map.get(num) + 1 )
+    let count1 = 0;
+    let count2 = 0;
+
+    for (let num of nums) {
+        if (num === candidate1) {
+            count1++
+        } else if (num === candidate2) {
+            count2++
+        } else if (count1 === 0) {
+            candidate1 = num
+            count1++
+        } else if (count2 === 0) {
+            candidate2 = num
+            count2++
         } else {
-            map.set(num, 1)
-        }
-
-        if(map.get(num) === majority){
-            res.push(num)
+            count1--;
+            count2--
         }
     }
+
+    let freq1 = 0;
+    let freq2 = 0;
+
+    for(let num of nums){
+        if(candidate1 === num){
+            freq1++
+        } else if(candidate2 === num){
+            freq2++
+        }
+    }
+
+    let res = []
+    const majority = Math.floor(nums.length / 3)
+    if(freq1 > majority ) res.push(candidate1)
+    if(freq2 > majority) res.push(candidate2)
 
     return res
 };
